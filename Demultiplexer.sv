f61d9098bb33bccb2232b382e3df8e1f345ac29a
jakemckenzie <jake314@uw.edu>
2018-05-29 00:33:21 -0700

    demultiplexer needs work:  Can't mix packed and unpacked types.

--- Demultiplexer.sv
+++ Demultiplexer.sv
@@ -13,3 +13,24 @@ module Demultiplexer#(parameter WIDTH,
             end
         endgenerate
 endmodule
+
+module Demultiplexer_testbench();
+    
+    parameter WIDTH = 8, SELECT_WIDTH = 5;
+    
+    logic [WIDTH - 1 : 0]data_in[2**SELECT_WIDTH];
+    logic [SELECT_WIDTH - 1 : 0]index;
+    logic [WIDTH - 1 : 0]data_out;
+
+    Demultiplexer #(WIDTH, SELECT_WIDTH) DUT(data_in, index, data_out);
+    
+    initial begin
+    
+        for (integer j = 0; j < WIDTH; j++) begin
+            data_in[j] = j;
+        end
+        for (integer i = 0; i < WIDTH; i++) begin
+            index = i;#1000;
+        end
+    end
+endmodule
