c796bdc29fcb75634148ae2501b4a02c3be4a4d6
Ammon Dodson <ammon@ammondodson.info>
2018-05-30 14:50:45 -0700

    generic memory.

---
+++ Mem.sv
@@ -0,0 +1,44 @@
+/* Authors:			Ammon Dodson & Jake McKenzie
+ * Date:		    Jun 6, 2018
+ * Description:     Data memory module
+ */
+
+
+module Mem #(parameter WIDTH, D_ADDR_WIDTH) (
+	input  clk, write,
+	input  [D_ADDR_WIDTH-1:0] addr,
+	input  [WIDTH-1:0] dataWrite,
+	output [WIDTH-1:0] dataRead
+);
+	logic [WIDTH-1:0] mem[2**D_ADDR_WIDTH-1:0];
+	
+	always_ff @(posedge clk) if(write) mem[addr] = dataWrite;
+	
+	assign dataRead = mem[addr];
+	
+endmodule
+
+
+module Mem_tb;
+	localparam WIDTH=4, D_ADDR_WIDTH=3;
+	logic  clk, write;
+	logic  [D_ADDR_WIDTH-1:0] addr;
+	logic  [WIDTH-1:0] dataWrite;
+	logic  [WIDTH-1:0] dataRead;
+	
+	
+	Mem #(WIDTH, D_ADDR_WIDTH) DUT(clk, write, addr, dataWrite, dataRead);
+	
+	initial begin
+		write=0;
+		addr =0;
+		dataWrite=0;
+		
+		clk=0; #10; clk=1; #10;
+		write=1;
+		clk=0; #10; clk=1; #10;
+		write=0;
+		
+	end
+endmodule
+
