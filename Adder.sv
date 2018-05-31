2fe6dd9135d11d1ce03f7e2daed00215af3eb2d1
Ammon Dodson <ammon@ammondodson.info>
2018-05-30 17:25:01 -0700

    Adder and ALU pass assertions.

--- Adder.sv
+++ Adder.sv
@@ -1,12 +1,9 @@
 module Adder #(WIDTH) (
 	input sub,
 	input[WIDTH-1:0] A, B,
-	output logic [WIDTH-1:0] Q,
-	output logic  CF, OF
+	output logic [WIDTH:0] Q,
+	output logic OF
 );
-	
-	
-	
 	always_comb begin
 		if(sub) OF = (!A[WIDTH-1] &  B[WIDTH-1] &  Q[WIDTH-1]) |
 		             ( A[WIDTH-1] & !B[WIDTH-1] & !Q[WIDTH-1]);
@@ -14,78 +11,79 @@ module Adder #(WIDTH) (
 		             ( A[WIDTH-1] &  B[WIDTH-1] & !Q[WIDTH-1]);
 	end
 	
-	
-	
 	always_comb case(sub)
-		0: {CF,Q} = A+B; // add
-		1: {CF,Q} = A-B; // sub
+		0: Q = A+B; // add
+		1: Q = A-B; // sub
 	endcase
-	
-	
 endmodule
 
 
 module Adder_tb;
 	localparam WIDTH=2;
+	localparam WIDER= WIDTH+1;
 	
 	logic sub;
 	logic[WIDTH-1:0] A, B;
-	logic[WIDTH-1:0] Q;
-	logic cf, of;
+	logic[WIDTH:0] Q;
+	logic of;
 	
-	Adder #(WIDTH) DUT(sub, A, B, Q, cf, of);
+	Adder #(WIDTH) DUT(sub, A, B, Q, of);
 	
 	initial begin
+		
+		// addition
 		for(int i=0; i<2**WIDTH; i++) for(int j=0; j<2**WIDTH; j++) begin
 			A=i; B=j;
 			sub=0; // add
 			#10;
-			assert(       i+j  == {cf,Q});
-			assert(WIDTH'(i+j) ==     Q );
+			assert(       i+j  ==         Q );
+			assert(WIDTH'(i+j) ==  WIDTH'(Q));
 			
 			if(of) begin
-				assert(3'(signed'(A)) + 3'(signed'(B)) != signed'(Q));
-				$display("%d+%d!=%d\t%d=%d",
+				assert(WIDER'(signed'(A)) + WIDER'(signed'(B)) !=
+					signed'(WIDTH'(Q)));
+				$display("%d+%d!=%d\t%d!=%d",
 					signed'(A), signed'(B), signed'(Q),
-					signed'(3'(A)+3'(B)), signed'(Q),
+					signed'(WIDER'(A)+WIDER'(B)), signed'(WIDTH'(Q)),
 				);
 			end
 			
 			
-			if(3'(signed'(A)) + 3'(signed'(B)) != signed'(Q)) begin
+			if(WIDER'(signed'(A)) + WIDER'(signed'(B)) != signed'(WIDTH'(Q))) begin
 				assert(of);
-				$display("%d+%d!=%d\t%d=%d",
+				$display("%d+%d!=%d\t%d!=%d",
 					signed'(A), signed'(B), signed'(Q),
-					signed'(3'(A)+3'(B)), signed'(Q),
+					signed'(WIDER'(A)+WIDER'(B)), signed'(WIDTH'(Q)),
 				);
 			end
 		end
 		
+		// subtract
 		for(int i=0; i<2**WIDTH; i++) for(int j=0; j<2**WIDTH; j++) begin
 			A=i; B=j;
-			sub=1; // add
+			sub=1; // sub
 			#10;
-			assert( signed'(i)- signed'(j)  == signed'({cf,Q}));
-			assert(WIDTH'(i-j) ==     Q );
+			assert( signed'(i)- signed'(j)  == signed'(Q));
+			assert(WIDTH'(i-j) ==  WIDTH'(Q));
 			
 			if(of) begin
-				assert(3'(signed'(A)) - 3'(signed'(B)) != signed'(Q));
-				$display("%d-%d!=%d\t%d=%d",
+				assert(WIDER'(signed'(A)) - WIDER'(signed'(B)) !=
+					signed'(WIDTH'(Q)));
+				$display("%d-%d!=%d\t%d!=%d",
 					signed'(A), signed'(B), signed'(Q),
-					signed'(3'(A)-3'(B)), signed'(Q),
+					signed'(WIDER'(A)-WIDER'(B)), signed'(WIDTH'(Q)),
 				);
 			end
 			
 			
-			if(3'(signed'(A)) - 3'(signed'(B)) != signed'(Q)) begin
+			if(WIDER'(signed'(A)) - WIDER'(signed'(B)) != signed'(WIDTH'(Q))) begin
 				assert(of);
-				$display("%d-%d!=%d\t%d=%d",
+				$display("%d-%d!=%d\t%d!=%d",
 					signed'(A), signed'(B), signed'(Q),
-					signed'(3'(A)-3'(B)), signed'(Q),
+					signed'(WIDER'(A)-WIDER'(B)), signed'(WIDTH'(Q)),
 				);
 			end
 		end
-		
 	end
 endmodule
 
